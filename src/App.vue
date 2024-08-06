45b32ece80fe45aa83fb5181db289e3cb9c9bf86
zhangdapeng520 <1156956636@qq.com>
2024-08-06 20:14:43 +0800

    优化：移除不必要的依赖

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@ import PatchMeta from './components/PatchMeta.vue'
 import Loader from './components/Loader.vue'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
-import 'bootstrap/dist/css/bootstrap.css'
 
 const blogSections = inject('blogSections', {})
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,41 +14,5 @@
 </template>
 
 <script setup lang="ts">
-import JSZip from 'jszip';
-import {saveAs} from 'file-saver';
 
-const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-// @ts-ignore | ts bug: https://github.com/microsoft/TypeScript/issues/40806
-const todayStr = new Date().toLocaleDateString("en-US", options);
-
-const text = `# 请填写博客标题 😎
-## 副标题
-
-> 一些内容
-
-其他的内容。。。
-`;
-
-const onSave = (text: string, html: string) => {
-  const postTitle = `post_${todayStr.replace(/,/mg, '').replace(/\s/mg, '_')}`;
-  const zip = new JSZip();
-  zip.file(`${postTitle}_source.md`, text);
-  zip.file(`${postTitle}_content.html`, `<!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-      </head>
-      <body>
-        <div class="container p-5 mx-auto">
-          ${html}
-        </div>
-      </body>
-    </html>`);
-  zip.generateAsync({type: "blob"})
-      .then(function (content) {
-        // see FileSaver.js
-        saveAs(content, `${postTitle}.zip`);
-      });
-}
 </script>
